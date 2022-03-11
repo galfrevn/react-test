952268a7831cecb8f8136476a4c97305f38c8d00
Francogalfre <galfre.vn@gmail.com>
2022-03-11 19:55:03 -0300

    FIx link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="w-screen h-20 bg-indigo-500 flex justify-between px-4 md:px-8 lg:px-12 shadow-lg items-center mb-10">
       <h1 className="text-white font-semibold lg:text-xl">Status dashboard</h1>
-      <a href="https://linkedin/in/galfrevn" className="text-white text-xs ">Valentín Galfré</a>
+      <a href="https://linkedin.com/in/galfrevn" className="text-white text-xs ">Valentín Galfré</a>
     </div>
   );
 };
